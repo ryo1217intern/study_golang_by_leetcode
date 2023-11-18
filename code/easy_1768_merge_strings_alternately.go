862514ac9fb4dbc0a7e61fcf227212839138197f
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-18 21:47:51 +0900

    1768の問題の作成。処理は未実装。

---
+++ code/easy_1768_merge_strings_alternately.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=1768 lang=golang
+ *
+ * [1768] Merge Strings Alternately
+ */
+package code
+
+// @lc code=start
+func mergeAlternately(word1 string, word2 string) string {
+	
+}
+// @lc code=end
+
