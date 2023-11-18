5ba54c09de7779f58e337fcc6d2fd661167499a1
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-18 12:40:32 +0900

    easy_1_two_sumの問題を処理した

--- code/easy_1_two_sum.go
+++ code/easy_1_two_sum.go
@@ -4,9 +4,23 @@
  * [1] Two Sum
  */
 package code
+
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-    
+	//辞書型配列の作成.要素はまだ入っていない.
+	numsMap := make(map[int]int)
+
+	//for rangeによってnumsの要素とそのindexを抽出しつつ要素の数だけfor文を回す.
+	for numsIndex, numsValue := range nums {
+		//numsMapに登録された値と一致するnumsMap[target-numsValue]が存在する場合numsMap[target-numsValue]は1を出力する.ない時は0を出力する.
+		if numsMap[target - numsValue] != 0 {
+			return []int{numsMap[target - numsValue] - 1, numsIndex}
+		}
+		//if文の条件式がfalseの際はnumsMapにその値を登録する.
+		numsMap[numsValue] = numsIndex + 1
+	}
+
+	return 	[]int{}
+
 }
 // @lc code=end
-
