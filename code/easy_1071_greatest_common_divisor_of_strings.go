67275085dc1d56931ada544f993dcaec92f8ade3
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-19 10:45:26 +0900

    1071の問題を作成.

---
+++ code/easy_1071_greatest_common_divisor_of_strings.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1071 lang=golang
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+// @lc code=start
+func gcdOfStrings(str1 string, str2 string) string {
+    
+}
+// @lc code=end
+
