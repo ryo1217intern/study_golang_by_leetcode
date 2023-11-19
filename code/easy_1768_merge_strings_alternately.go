f9e30affb933ef873511c4147282306aac75b6de
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-19 10:43:44 +0900

    1768の問題を処理した

--- code/easy_1768_merge_strings_alternately.go
+++ code/easy_1768_merge_strings_alternately.go
@@ -7,7 +7,33 @@ package code
 
 // @lc code=start
 func mergeAlternately(word1 string, word2 string) string {
-	
+
+	//仮にword1が最小の文字数として最小の文字数を代入
+	minLength := len(word1)
+	//残りのテキストを初期化
+	remainText := ""
+	//リターンする値の初期化
+	resultText := ""
+
+	//word2が最小の文字の際の処理を記述
+	if len(word1) > len(word2) {
+		minLength = len(word2)
+		//ここで初めて残りのテキストに文字列を代入する
+		remainText = word1[minLength:]
+	} else {
+		//word1が最小の場合remainTextはword2の残りとなる
+		remainText = word2[minLength:]
+	}
+
+	//文字を順番に足していく
+	for i := 0; i < minLength; i++ {
+		resultText += string(word1[i]) + string(word2[i])
+	}
+
+	//残った文字をresultTextに追加する
+	resultText += remainText
+
+	return resultText
 }
 // @lc code=end
 
